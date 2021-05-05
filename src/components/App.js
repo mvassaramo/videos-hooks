7fc653428b44851e53656ca99a213a6cf4db69e7
Maduri Vassaramo <maduri@vinterior.co>
2021-05-05 10:03:52 +0200

    implement GET request to youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  onFormSubmit = (term) => {
+    youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+  }
+
   render() {
     return(
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onFormSubmit}/>
       </div>
     )
   }
