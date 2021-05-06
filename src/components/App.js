f556ec57a88142a84fb0cc201b2258c271c3aa8e
Maduri Vassaramo <maduri@vinterior.co>
2021-05-06 08:30:14 +0200

    add state to App to store videos

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onFormSubmit = (term) => {
-    youtube.get('/search', {
+  state= { videos: [] }
+
+  onFormSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
+
+    this.setState({ videos: response.data.items })
   }
 
   render() {
     return(
       <div className="ui container">
         <SearchBar onFormSubmit={this.onFormSubmit}/>
+        I have {this.state.videos.length} videos
       </div>
     )
   }
