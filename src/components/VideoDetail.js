0e11a8d7a344e6a1271c05693c046ea3ad90eb97
Maduri Vassaramo <maduri@vinterior.co>
2021-05-07 10:00:23 +0200

    add some styling to VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,8 @@ const VideoDetail = ({ video }) => {
   return(
     <div>
       <img src={video.snippet.thumbnails.high.url} alt=""/>
-      <div>
-        <h4>{video.snippet.title}</h4>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
       </div>
     </div>
